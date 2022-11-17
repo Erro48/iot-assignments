d83cbe4f6428af2796556b49934b1078130c0814
Marco Sternini <mkarko01@gmail.com>
2022-11-17 11:29:59 +0100

    Add pins

--- assignment-02/arduino/include/constants.h
+++ assignment-02/arduino/include/constants.h
@@ -1,16 +1,22 @@
 #ifndef __CONSTANTS__
 #define __CONSTANTS__
 
-//pins
+/* PINS */
+/* -Sensors */
 #define P_SONAR_TRIG 7
 #define P_SONAR_ECHO 8
 #define P_PIR 9
+#define P_BUTTON 11
 #define P_PHOTORESISTOR A0
-#define P_LED_A 4
-#define P_LED_B 3
+#define P_POT A1
+
+/* -Actuators */
+#define P_LED_A 3
+#define P_LED_B 4
 #define P_LED_C 5
+#define P_MOTOR 10
 
-
+/* CONSTANTS */
 #define MAX_TASKS 50
 #define MIN_LUM 650
 #define TRIG_DELAY 3
