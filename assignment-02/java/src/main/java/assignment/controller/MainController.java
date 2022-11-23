691ceed803a8c6d26c4f93d98ed650b9fb0299c9
Marco Sternini <mkarko01@gmail.com>
2022-11-23 17:51:52 +0100

    Fix gradle tasks
    - Now 'run' and 'shadowJar' tasks work
    - Add javadoc

--- assignment-02/java/src/main/java/assignment/controller/MainController.java
+++ assignment-02/java/src/main/java/assignment/controller/MainController.java
@@ -15,12 +15,22 @@ public interface MainController {
      */
     void setPort(String port);
     
-    
+    /** 
+     * Moves the servo
+     * @param angle
+     *          The new angle
+     */
     void sendMotorAngle(int angle);
     
-    
+    /**
+     * Switch to manual/auto mode
+     */
     void toggleMode();
     
-    
+    /**
+     * Handle error messages sent by Arduino
+     * @param message
+     *          The error message with an optional code [01]
+     */
     void handleError(String message);
 }
--- assignment-02/java/src/main/java/assignment/view/MainView.java
+++ assignment-02/java/src/main/java/assignment/view/MainView.java
@@ -2,14 +2,28 @@ package assignment.view;
 
 public interface MainView {
 
+    /**
+     * Add a record to the chart
+     * @param waterLevel
+     */
     void addData(int waterLevel);
     
-    
+    /**
+     * Resets the chart
+     */
     void deleteData();
     
-    
+    /**
+     * Show on the view an error
+     * @param message
+     *          The error message
+     */
     void showError(String message);
     
-
+    /**
+     * Changes the text of the control button 
+     * @param manual
+     *          The mode manual/auto
+     */
     void setButtonText(boolean manual);
 }
